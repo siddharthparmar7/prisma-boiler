8a53fdd88256bd56019bfc08ee16a08097909107
Sid Parmar <siddharthparmar7@gmail.com>
2019-11-01 16:13:41 -0700

    prisma boiler project

---
+++ src/resolvers/Query.js
@@ -0,0 +1,10 @@
+const { getUserId } = require("../utils");
+
+const Query = {
+  user(parent, args, context) {
+    const id = getUserId(context);
+    return context.prisma.user({ id });
+  },
+};
+
+module.exports = { Query };
